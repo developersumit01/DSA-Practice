f482c8dcf7b11c228eb9e3812adee9fb557ba25b
Sumit Kumar <sumitkumar84390@gmail.com>
2023-10-17 18:37:38 +0530

    Make a method for inplace marge sort

--- src/recursion/MargeSort.java
+++ src/recursion/MargeSort.java
@@ -6,11 +6,50 @@ public class MargeSort {
 	
 	public static void main(String[] args) {
 		MargeSort margeSort=new MargeSort();
-		int[] arr= {};
-		arr=margeSort.margeSort(arr);
+		int[] arr= {5,4,3,2,1};
+		margeSort.inplaceMargeSort(arr,0,arr.length);
 		System.out.println(Arrays.toString(arr));
 	}
 	
+	public void inplaceMargeSort(int[] arr,int start,int end) {
+		if((end-start)==1) {
+			return;
+		}
+		int mid=(end+start)/2;
+		inplaceMargeSort(arr, start, mid);
+		inplaceMargeSort(arr, mid, end);
+		inplaceMarge(arr, start, mid,end);
+	}
+	
+	private void inplaceMarge(int[] arr, int start, int mid,int end) {
+		int[] marge=new int[end-start];
+		int i=start,j=mid,k=0;
+		while(i<mid&&j<end) {
+			if(arr[i]>arr[j]) {
+				marge[k]=arr[j];
+				j++;
+			}else {
+				marge[k]=arr[i];
+				i++;
+			}
+			k++;
+		}
+		
+		while(i<mid) {
+			marge[k]=arr[i];
+			i++;
+			k++;
+		}
+		while(j<end) {
+			marge[k]=arr[j];
+			j++;
+			k++;
+		}
+		for(int l=0;l<marge.length;l++) {
+			arr[start+l]=marge[l];
+		}
+	}
+
 	public int[] margeSort(int[] arr) {
 		if(arr.length==1||arr.length==0) {
 			return arr;
