5a1d360129d3120361c1a77e9a068f214065a616
Sumit Kumar <sumitkumar84390@gmail.com>
2023-10-23 11:37:37 +0530

    Add a method which check an array can divide into two sub array which have same prefix sum

--- src/arrayPrograms/PrefixSum.java
+++ src/arrayPrograms/PrefixSum.java
@@ -1,13 +1,36 @@
 package arrayPrograms;
 
-import java.util.Arrays;
-
 public class PrefixSum {
+	
+	public static void main(String[] args) {
+		PrefixSum prefixSum=new PrefixSum();
+		int[] arr= {1,2,3,5,1};
+		System.out.println(prefixSum.subArrayOrNot(arr));
+	}
 	
 	public void prefixSum(int[] arr) {
 		for(int i=1;i<arr.length;i++) {
 			arr[i]=arr[i]+arr[i-1];
 		}
 	}
-	
+//	Find the array can be divide into to sub part or not 
+//	in which both prefix sum should be equal.
+	public boolean subArrayOrNot(int[] arr) {
+		int start=0,end=arr.length-1;
+		int leftPrifixSum=0,rightPrefixSum=0;
+		while(start<=end) {
+			if (leftPrifixSum<=rightPrefixSum) {
+				leftPrifixSum+=arr[start];
+				start++;
+			}else {
+				rightPrefixSum+=arr[end];
+				end--;
+			}
+		}
+		if(leftPrifixSum==rightPrefixSum) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 }
