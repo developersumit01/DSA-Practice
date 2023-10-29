8aeab805b8e57e05de03489fcf205b785233482d
SUMIT KUMAR <145597487+sumit01developer@users.noreply.github.com>
2023-10-29 21:08:59 +0530

    Add new line in hello.java for check something
    I am making something changes for you so you can check what you want.

--- src/practiceQusetion/HelloWorld.java
+++ src/practiceQusetion/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Welcome to GitHub");
+		System.out.println("Welcome Sumit Kumar");
 	}
 }
